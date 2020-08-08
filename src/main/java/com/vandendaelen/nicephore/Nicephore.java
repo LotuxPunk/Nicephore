890713e997de67eaba1359f6641298b13644a529
LotuxPunk <vandendaelenclement@gmail.com>
2020-08-08 12:54:56 +0200

    Configuration to set compression

--- src/main/java/com/vandendaelen/nicephore/Nicephore.java
+++ src/main/java/com/vandendaelen/nicephore/Nicephore.java
@@ -1,8 +1,11 @@
 package com.vandendaelen.nicephore;
 
+import com.vandendaelen.nicephore.config.NicephoreConfig;
 import com.vandendaelen.nicephore.event.ClientEvents;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
@@ -12,12 +15,13 @@ import org.apache.logging.log4j.Logger;
 @Mod(Nicephore.MODID)
 public class Nicephore {
     // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
+    public static final Logger LOGGER = LogManager.getLogger();
     public static final String MODID = "nicephore";
     public static final String MOD_NAME = "Nicephore";
 
     public Nicephore() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, NicephoreConfig.CLIENT_SPEC);
         MinecraftForge.EVENT_BUS.register(this);
         System.setProperty("java.awt.headless", "false");
     }
---
+++ src/main/java/com/vandendaelen/nicephore/config/NicephoreConfig.java
@@ -0,0 +1,30 @@
+package com.vandendaelen.nicephore.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import org.apache.commons.lang3.tuple.Pair;
+
+public class NicephoreConfig {
+    public static final Client CLIENT;
+    public static final ForgeConfigSpec CLIENT_SPEC;
+
+    static {
+        Pair<Client, ForgeConfigSpec> specClientPair = new ForgeConfigSpec.Builder().configure(Client::new);
+        CLIENT_SPEC = specClientPair.getRight();
+        CLIENT = specClientPair.getLeft();
+    }
+
+    public static class Client {
+        public final ForgeConfigSpec.DoubleValue compression;
+
+        public Client(ForgeConfigSpec.Builder builder) {
+            builder.push("Client settings");
+            compression = builder
+                    .comment("Compression level")
+                    .defineInRange("compression", 0.9, 0.0,1.0);
+            builder.pop();
+        }
+        public static float getCompressionLevel() {
+            return CLIENT.compression.get().floatValue();
+        }
+    }
+}
