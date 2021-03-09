ef68be138ad9947e0117ecd436a9ab9f66f25b1a
Vandendaelen <vandendaelenclement@gmail.com>
2021-03-09 13:43:59 +0100

    Fixed an issue with .toml.bak extending the config file.

--- src/main/java/com/vandendaelen/nicephore/config/NicephoreConfig.java
+++ src/main/java/com/vandendaelen/nicephore/config/NicephoreConfig.java
@@ -22,18 +22,14 @@ public final class NicephoreConfig {
         public Client(ForgeConfigSpec.Builder builder) {
             builder.push("Client settings");
             compression = builder
-                    .comment("JPEG compression level, the higher the number, the better the quality." +
-                            "\r\nNote that 1.0 is *not* lossless as JPEG is a lossy-only format, use the PNG files instead if you want lossless.")
+                    .comment("JPEG compression level, the higher the number, the better the quality.", "Note that 1.0 is *not* lossless as JPEG is a lossy-only format, use the PNG files instead if you want lossless.")
                     .defineInRange("compression", 0.9, 0.0,1.0);
             makeJPEGs = builder
-                    .comment("Enable to allow Nicephore to make lossy JPEGs of your screenshots for easier online sharing. Disable to only allow PNGs." +
-                            "\r\nNote that PNGs will still be made regardless of this option.")
+                    .comment("Enable to allow Nicephore to make lossy JPEGs of your screenshots for easier online sharing. Disable to only allow PNGs.", "Note that PNGs will still be made regardless of this option.")
                     .define("makeJPEGs", true);
 
             optimisedOutput = builder
-                    .comment("Enable to allow Nicephore to losslessly optimise the PNG and JPEG screenshots for smaller sized progressive files that are of identical quality to the files before optimisation." +
-                            "\r\nNote: Enabling this will cause screenshots to take slightly longer to save as an optimisation step will have to be run first." +
-                            "\r\nTip: In the rare case that a screenshot PNG is corrupted, run \"oxipng --fix (filename).png\" to attempt to fix it.")
+                    .comment("Enable to allow Nicephore to losslessly optimise the PNG and JPEG screenshots for smaller sized progressive files that are of identical quality to the files before optimisation.", "Note: Enabling this will cause screenshots to take slightly longer to save as an optimisation step will have to be run first.", "Tip: In the rare case that a screenshot PNG is corrupted, run \"oxipng --fix (filename).png\" to attempt to fix it.")
                     .define("optimiseScreenshots", Util.getOS().equals(Util.OS.MAC) || Util.getOS().equals(Util.OS.WINDOWS));
 
             showOptimisationStatus = builder
@@ -42,8 +38,7 @@ public final class NicephoreConfig {
 
             builder.push("PNG-specific settings");
             pngOptimisationLevel = builder
-                    .comment("If optimiseScreenshots is enabled, use the following oxipng optimisation level, with higher numbers taking longer to process but give lower file sizes." +
-                            "\r\nTip: I would avoid anything above 3 unless you have a lot of CPU cores and threads (e.g. 16c/32t+) as it starts taking significantly longer to process for vastly diminishing returns.")
+                    .comment("If optimiseScreenshots is enabled, use the following oxipng optimisation level, with higher numbers taking longer to process but give lower file sizes.", "Tip: I would avoid anything above 3 unless you have a lot of CPU cores and threads (e.g. 16c/32t+) as it starts taking significantly longer to process for vastly diminishing returns.")
                     .defineInRange("pngOptimisationLevel", 2, 0, 5);
             builder.pop();
 
