282b6b38f7cb3d21590cee9d3a43d1ebfb62adcd
Clément Vandendaelen <vandendaelenclement@gmail.com>
2021-05-21 17:51:31 +0200

    Fix #14

--- src/main/java/com/vandendaelen/nicephore/Nicephore.java
+++ src/main/java/com/vandendaelen/nicephore/Nicephore.java
@@ -3,12 +3,15 @@ package com.vandendaelen.nicephore;
 import com.vandendaelen.nicephore.config.NicephoreConfig;
 import com.vandendaelen.nicephore.event.ClientEvents;
 import com.vandendaelen.nicephore.thread.InitThread;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -24,6 +27,8 @@ public final class Nicephore {
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, NicephoreConfig.CLIENT_SPEC);
         MinecraftForge.EVENT_BUS.register(this);
         System.setProperty("java.awt.headless", "false");
+
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> "", (a, b) -> true));
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
