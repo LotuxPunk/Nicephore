a2279f00760e5e72c3c095c66732f67ba2ee0034
Clément VANDENDAELEN <cv@icure.com>
2026-03-31 22:01:19 +0200

    fix: add @Volatile to mutable Reference fields for thread safety

--- src/main/kotlin/com/vandendaelen/nicephore/utils/Reference.kt
+++ src/main/kotlin/com/vandendaelen/nicephore/utils/Reference.kt
@@ -7,22 +7,28 @@ object Reference {
     const val ECT_EXE: String = "ect-0.8.3.exe"
 
     object Command {
+        @Volatile
         @JvmField
         var OXIPNG: String = ""
+        @Volatile
         @JvmField
         var ECT: String = ""
     }
 
     object File {
+        @Volatile
         @JvmField
         var OXIPNG: String = ""
+        @Volatile
         @JvmField
         var ECT: String = ""
     }
 
     object Version {
+        @Volatile
         @JvmField
         var OXIPNG: String = ""
+        @Volatile
         @JvmField
         var ECT: String = ""
     }
